cba222edbd50829b21158262c5f28b1b8cada474
Giove93 <gioschy93@gmail.com>
2021-01-14 00:05:18 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingUpComponent } from './components/sing-up/sing-up.component';
+import { RicercaComponent } from './components/ricerca/ricerca.component';
+import { PianetiComponent } from './components/pianeti/pianeti.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SingUpComponent,
+    RicercaComponent,
+    PianetiComponent
   ],
   imports: [
     BrowserModule,
